58062aab8656f3f7b1b9dc0f43df7e4783a4355e
rohankhatik <rc7354463@gmail.com>
2026-04-07 17:59:04 +0530

    Added signup API with user creation and signals

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt
+import json
+from django.contrib.auth.models import User
 
-# Create your views here.
+from django.http import JsonResponse
+
+
+@csrf_exempt
+def signup(request):
+    if request.method == 'POST':
+        data=json.loads(request.body)
+
+        username=data.get('username')
+        password=data.get('password')
+        
+        User.objects.create_user(
+            username=username, password=password )
+
+
+
+
+        return JsonResponse({'message': 'user created'})
+    
+    return JsonResponse({'message': 'error, only post working'})
